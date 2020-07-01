adb0f4c3a8af4aa48bd1e73fdd30095a641c4c98
yuanhangpeng <15162149191@163.com>
2020-07-01 18:21:49 +0800

    [*]提交

---
+++ docs/.vuepress/components/button/demo-group.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="list">
+           <vr-button-group>
+               <vr-button type="primary">按钮1</vr-button>
+               <vr-button type="primary">按钮2</vr-button>
+           </vr-button-group>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Button from '../../../../packages/button/src/button'
+    import ButtonGroup from '../../../../packages/button/src/button-group'
+    export default {
+        name: 'button-demo-group',
+        components: {
+            [Button.name]: Button,
+            [ButtonGroup.name]:ButtonGroup
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .list{
+        margin: 10px 0;
+    }
+</style>
